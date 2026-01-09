3cb9d10dcdb12d7505494a985bd72c776196cc39
Wagner <wagnerdossantos2311@gmail.com>
2026-01-09 16:19:21 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+import db.DB;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
